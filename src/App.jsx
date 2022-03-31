79072f152c1cf44b698021033cd5627182e8254a
natsumi <aka.723n@gmail.com>
2022-03-31 13:48:04 +0900

    ボタン追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 
 
 export const App = () =>{
+  const onClickButton = () => {
+    alert ();
+  };
   return (
     <>
       <h1>こんにちは</h1>
+      <p>お元気ですか</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 
