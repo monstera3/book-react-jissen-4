2c07d64241f51e439a1da9dfb5a8d2a9d1523e5a
natsumi <aka.723n@gmail.com>
2022-03-31 16:40:22 +0900

    state追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { ColoredMessage } from "./components/ColoredMessage";
+import {useState} from "react";
 
 export const App = () =>{
+
+  //stateの定義
+  const [num, setNum] = useState(0);
+
   const onClickButton = () => {
-    alert ();
+    setNum(num + 1);
   };
-  
+
   return (
     <>
       <h1 style={{color:"red"}}>こんにちは</h1>
       <ColoredMessage color="blue">お元気ですか</ColoredMessage>
       <ColoredMessage color="pink">元気です</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 
